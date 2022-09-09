6bc7cd6b3571905c0398dcc9a5b96563fbd29e89
nmhieu2201 <minhhieudz.22012000a@gmail.com>
2022-09-09 23:02:33 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const inputBox = document.querySelector("#newTask");
 const addBtn = document.querySelector("#addItem");
 const todoList = document.querySelector("#todo");
-let listArr;
+const todoCompleted = document.querySelector("#completed");
+let listArr = [];
+
 function validateData(value) {
   let valid = false;
   if (value.trim() !== "") {
@@ -10,8 +12,8 @@ function validateData(value) {
   return valid;
 }
 addBtn.addEventListener("click", (event) => {
-  let userData = inputBox.value;
-  const isValid = validateData(userData);
+  let taskName = inputBox.value;
+  const isValid = validateData(taskName);
   if (!isValid) {
     return;
   }
@@ -21,7 +23,11 @@ addBtn.addEventListener("click", (event) => {
   } else {
     listArr = JSON.parse(getLocalStorage);
   }
-  listArr.push(userData);
+  listArr.push({
+    task: taskName,
+    checked: false,
+    id: Math.random().toString(16).slice(2),
+  });
   localStorage.setItem("todo", JSON.stringify(listArr));
   showTask();
 });
@@ -39,19 +45,34 @@ const showTask = () => {
     listArr = JSON.parse(getLocalStorage);
   }
   let newTag = "";
+  let completedTask = "";
   listArr.forEach((ele, index) => {
-    newTag += `
-            <li>
-                ${ele}
-                <div class="icon">
-                <i style="cursor: pointer" onclick="handleDeleleItem('${index}')"; class="fa-solid fa-trash-can"></i>
-                <i style="cursor: pointer" onclick="handleCheckItem('${index}');" class="fa-regular fa-circle-check"></i>
-                </div>
-            </li>
+    if (ele.checked) {
+      completedTask += `
+      <li>
+        ${ele.task}
+        <div class="icon">
+        <i style="cursor: pointer" onclick="handleDeleleItem('${index}')"; class="fa-solid fa-trash-can"></i>
+        <i style="color:green ;"  onclick="handleCheckItem('${ele.id}');"  class="fa-solid fa-circle-check"></i>
+        </div>
+      </li>
+      `;
+    } else {
+      newTag += `
+      <li>
+          ${ele.task}
+          <div class="icon">
+          <i style="cursor: pointer" onclick="handleDeleleItem('${index}')"; class="fa-solid fa-trash-can"></i>
+          <i style="cursor: pointer" onclick="handleCheckItem('${ele.id}');" class="fa-regular fa-circle-check"></i>
+          </div>
+      </li>
 
-        `;
+  `;
+    }
   });
   todoList.innerHTML = newTag;
+  todoCompleted.innerHTML = completedTask;
+  console.log(completedTask);
   inputBox.value = "";
 };
 showTask();
@@ -62,4 +83,13 @@ const handleDeleleItem = (index) => {
   localStorage.setItem("todo", JSON.stringify(listArr));
   showTask();
 };
-const handleCheckItem = (index) => {};
+const handleCheckItem = (id) => {
+  console.log(id);
+  listArr.forEach((el) => {
+    if (el.id === id) {
+      el.checked = !el.checked;
+    }
+  });
+  console.log(listArr);
+  showTask();
+};
